2ab783e5087504dddea4263828d4680fb91dee41
David Yoon <yoon.kyungtaek@gmail.com>
2025-10-31 15:13:12 -0400

    assignment 4 added

---
+++ Assignment04/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Assignment04;
+
+public interface IRepository<T>
+{
+    //CRUD
+    public void Add(T item);
+    public T GetById(int id);
+    public IEnumerable<T> GetAll();
+    public void Save();
+    public void Remove(T item);
+}
---
+++ Assignment04/MyList.cs
@@ -0,0 +1,42 @@
+﻿namespace Assignment04;
+
+public class MyList<T>
+{
+    private List<T> list = new List<T>();
+    public void Add(T element)
+    {
+        list.Add(element);
+    }
+
+    public T Remove(int index)
+    {
+        T element = list[index];
+        list.RemoveAt(index);
+        return element;
+    }
+
+    public bool Contains(T element)
+    {
+        return list.Contains(element);
+    }
+
+    public void Clear()
+    {
+        list.Clear();
+    }
+
+    public void InsertAt(T element, int index)
+    {
+        list.Insert(index, element);
+    }
+
+    public void DeleteAt(int index)
+    {
+        list.RemoveAt(index);
+    }
+
+    public T Find(int index)
+    {
+        return list[index];
+    }
+}
