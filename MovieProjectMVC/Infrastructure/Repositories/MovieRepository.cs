1e450637446eed5471a6a12b7666443efbc7f772
David Yoon <yoon.kyungtaek@gmail.com>
2025-11-21 16:03:42 -0500

    MVC assignment 1

---
+++ MovieProjectMVC/Infrastructure/Repositories/MovieRepository.cs
@@ -0,0 +1,18 @@
+﻿using ApplicationCore.Contracts.Repositories;
+using ApplicationCore.Entities;
+using Infrastructure.Data;
+
+namespace Infrastructure.Repositories;
+
+public class MovieRepository: BaseRepository<Movie>, IMovieRepository
+{
+    public MovieRepository(MovieShopDbContext context) : base(context)
+    {
+    }
+
+    public IEnumerable<Movie> GetTop20GrossingMovies()
+    {
+        var movies = _context.Movies.OrderByDescending(m => m.Revenue).Take(20);
+        return movies;
+    }
+}
---
+++ MovieProjectMVC/Infrastructure/Services/MovieService.cs
@@ -0,0 +1,61 @@
+﻿using ApplicationCore.Contracts.Repositories;
+using ApplicationCore.Contracts.Services;
+using ApplicationCore.Models;
+
+namespace Infrastructure.Services;
+
+public class MovieService: IMovieService
+{
+    private readonly IMovieRepository _repository;
+    public MovieService(IMovieRepository movieRepository)
+    {
+        _repository = movieRepository;
+    }
+    public List<MovieCardModel> Top20GrossingMovie()
+    {
+        var movies = _repository.GetTop20GrossingMovies();
+        var movieCardModels = new List<MovieCardModel>();
+        foreach (var movie in movies)
+        {
+            movieCardModels.Add(new MovieCardModel()
+            {
+                Title = movie.Title,
+                Id = movie.Id,
+                PosterUrl = movie.PosterUrl
+            });
+        }
+        return movieCardModels;
+    }
+
+    public MovieDetailsModel GetMovieDetails(int id)
+    {
+        var movie = _repository.GetById(id);
+        if (movie == null)
+        {
+            return null;
+        }
+
+        var movieDetailsModel = new MovieDetailsModel()
+        {
+            Id = movie.Id,
+            Title = movie.Title,
+            PosterUrl = movie.PosterUrl,
+            Overview = movie.Overview,
+            Tagline = movie.Tagline,
+            Budget = movie.Budget,
+            Revenue = movie.Revenue
+        };
+        return movieDetailsModel;
+    }
+
+    public bool DeleteMovie(int id)
+    {
+        var movie = _repository.DeleteById(id);
+        if (movie == null)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
